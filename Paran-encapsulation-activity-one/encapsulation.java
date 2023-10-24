a362f61e3e8846c8f7748b729f775730519b1a7a
Pranxxx <148731846+Pranxxx@users.noreply.github.com>
2023-10-25 00:37:47 +0800

    Add files via upload

---
+++ Paran-encapsulation-activity-one/encapsulation.java
@@ -0,0 +1,26 @@
+public class encapsulation {
+ 
+    private String firstName;
+    private String middletName;
+    private String lastName;
+    private int age;
+    private int birthDay;
+    private int address;
+
+    public String getFirstName(){ return firstName;}
+    public String getMiddleName(){ return middletName;}
+    public String getLastName(){ return lastName;}
+    public int getAge(){ return age;}
+    public int getBirthDay(){ return birthDay;}
+    public int getAddress(){ return address;}
+
+    public void setFirstName(String firstName){ this.firstName = firstName;}
+    public void setMiddleName(String middletName){ this.middletName = middletName;}
+    public void setLastName(String lastName){ this.lastName = lastName;}
+    public void setAge(int age){ this.age = age;}
+    public void setBirthDay(int birthDay){ this.birthDay = birthDay;}
+    public void setAddress(int address){ this.address = address;}
+
+
+
+}
